ca0deb93df2689a9a292be52c008d2251541cec4
Artem Vorotnikov <artem@vorotnikov.me>
2018-02-15 19:50:04 +0300

    More net stuff

---
+++ http/src/request_util.rs
@@ -0,0 +1,39 @@
+use errors;
+
+use futures::prelude::*;
+use futures::future;
+use hyper;
+use serde_json;
+
+use serde::de::Deserialize;
+
+/// Transforms request body with the following pipeline:
+///
+///   1. Parse request body into entity of type T (T must implement `serde::de::Deserialize` trait)
+///
+///   2. Validate entity (T must implement `validator::Validate`)
+///
+/// Fails with `error::Error::UnprocessableEntity` if step 1 fails.
+///
+/// Fails with `error::Error::BadRequest` with message if step 2 fails.
+pub fn parse_body<T>(body: hyper::Body) -> Box<Future<Item = T, Error = errors::ControllerError>>
+where
+    T: for<'a> Deserialize<'a> + 'static,
+{
+    Box::new(
+        read_body(body)
+            .map_err(|err| errors::ControllerError::Parse(format!("{}", err)))
+            .and_then(|body| serde_json::from_str::<T>(&body).map_err(|e| errors::ControllerError::UnprocessableEntity(e.into()))),
+    )
+}
+
+/// Reads body of request and response in Future format
+pub fn read_body(body: hyper::Body) -> Box<Future<Item = String, Error = hyper::Error>> {
+    Box::new(body.fold(Vec::new(), |mut acc, chunk| {
+        acc.extend_from_slice(&*chunk);
+        future::ok::<_, hyper::Error>(acc)
+    }).and_then(|bytes| match String::from_utf8(bytes) {
+        Ok(data) => future::ok(data),
+        Err(err) => future::err(hyper::Error::Utf8(err.utf8_error())),
+    }))
+}

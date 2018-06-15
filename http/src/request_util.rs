83c155001d44688f875741a409d8ee511c30bbf5
Serejka <5125402@mail.ru>
2018-06-15 15:18:59 +0300

    added currency id header (#60)

--- http/src/request_util.rs
+++ http/src/request_util.rs
@@ -8,6 +8,7 @@ use serde::ser::Serialize;
 use serde_json;
 
 header! { (SessionId, "Session-Id") => [String] }
+header! { (CurrencyId, "Currency-Id") => [String] }
 
 #[derive(Clone, Debug, Fail)]
 pub enum ParseError {

df694cfd9a5dc845b5d3a3a36401dd2dfe34ba53
Artem Vorotnikov <artem@vorotnikov.me>
2018-07-17 17:29:14 +0300

    newtype macro

--- types/src/structs.rs
+++ types/src/structs.rs
@@ -4,58 +4,51 @@ use uuid::Uuid;
 
 use stq_static_resources::Currency;
 
-#[derive(Clone, Copy, Debug, Default, Display, PartialEq, Eq, PartialOrd, Ord, From, FromStr,
-         Into, Hash, Serialize, Deserialize, DieselTypes)]
-pub struct UserId(pub i32);
+macro_rules! f64_newtype {
+    ($x:ident) => {
+        #[derive(Clone, Copy, Debug, Display, Default, PartialEq, PartialOrd, From, FromStr,
+                 Into, Serialize, Deserialize, DieselTypes)]
+        pub struct $x(pub f64);
+    };
+}
+macro_rules! i32_newtype {
+    ($x:ident) => {
+        #[derive(Clone, Copy, Debug, Display, Default, PartialEq, Eq, PartialOrd, Ord, From,
+                 FromStr, Into, Hash, Serialize, Deserialize, DieselTypes)]
+        pub struct $x(pub i32);
+    };
+}
+macro_rules! string_newtype {
+    ($x:ident) => {
+        #[derive(Clone, Debug, Display, Default, PartialEq, Eq, PartialOrd, Ord, From, FromStr,
+                 Into, Hash, Serialize, Deserialize, DieselTypes)]
+        pub struct $x(pub String);
+    };
+}
+macro_rules! uuid_newtype {
+    ($x:ident) => {
+        #[derive(Clone, Copy, Debug, Default, Display, PartialEq, Eq, PartialOrd, Ord, From,
+                 FromStr, Into, Hash, Serialize, Deserialize, DieselTypes)]
+        pub struct $x(pub Uuid);
 
-#[derive(Clone, Copy, Debug, Default, Display, PartialEq, Eq, PartialOrd, Ord, From, FromStr,
-         Into, Hash, Serialize, Deserialize, DieselTypes)]
-pub struct RoleEntryId(pub Uuid);
-
-impl RoleEntryId {
-    pub fn new() -> Self {
-        RoleEntryId(Uuid::new_v4())
-    }
+        impl $x {
+            pub fn new() -> Self {
+                $x(Uuid::new_v4())
+            }
+        }
+    };
 }
 
-#[derive(Clone, Copy, Debug, Default, From, FromStr, Into, Display, Eq, PartialOrd, Ord,
-         PartialEq, Hash, Serialize, Deserialize, DieselTypes)]
-pub struct RoleId(pub Uuid);
+i32_newtype!(UserId);
+uuid_newtype!(RoleEntryId);
+uuid_newtype!(RoleId);
+i32_newtype!(ProductId);
+i32_newtype!(BaseProductId);
+i32_newtype!(Quantity);
+i32_newtype!(StoreId);
 
-impl RoleId {
-    pub fn new() -> Self {
-        RoleId(Uuid::new_v4())
-    }
-}
-
-#[derive(Clone, Copy, Debug, Display, Default, PartialEq, Eq, PartialOrd, Ord, From, FromStr,
-         Into, Hash, Serialize, Deserialize, DieselTypes)]
-pub struct ProductId(pub i32);
-
-#[derive(Clone, Copy, Debug, Display, Default, PartialEq, Eq, PartialOrd, Ord, From, FromStr,
-         Into, Hash, Serialize, Deserialize, DieselTypes)]
-pub struct BaseProductId(pub i32);
-
-#[derive(Clone, Copy, Debug, Display, Default, PartialEq, Eq, PartialOrd, Ord, From, FromStr,
-         Into, Hash, Serialize, Deserialize, DieselTypes)]
-pub struct Quantity(pub i32);
-
-#[derive(Clone, Copy, Debug, Display, Default, PartialEq, Eq, PartialOrd, Ord, From, FromStr,
-         Into, Hash, Serialize, Deserialize, DieselTypes)]
-pub struct StoreId(pub i32);
-
-#[derive(Clone, Copy, Debug, Display, Default, PartialEq, Eq, PartialOrd, Ord, From, FromStr,
-         Into, Hash, Serialize, Deserialize, DieselTypes)]
-pub struct WarehouseId(pub Uuid);
-impl WarehouseId {
-    pub fn new() -> Self {
-        WarehouseId(Uuid::new_v4())
-    }
-}
-
-#[derive(Clone, Debug, Display, Default, PartialEq, Eq, PartialOrd, Ord, From, FromStr, Into,
-         Hash, Serialize, Deserialize, DieselTypes)]
-pub struct WarehouseSlug(pub String);
+uuid_newtype!(WarehouseId);
+string_newtype!(WarehouseSlug);
 
 #[derive(Clone, Debug, PartialEq, Hash)]
 pub enum WarehouseIdentifier {
@@ -63,74 +56,13 @@ pub enum WarehouseIdentifier {
     Slug(WarehouseSlug),
 }
 
-#[derive(Clone, Copy, Debug, Display, Default, PartialEq, Eq, PartialOrd, Ord, From, FromStr,
-         Into, Hash, Serialize, Deserialize, DieselTypes)]
-pub struct StockId(pub Uuid);
-impl StockId {
-    pub fn new() -> Self {
-        StockId(Uuid::new_v4())
-    }
-}
-
-#[derive(Clone, Copy, Debug, Display, Default, PartialEq, Eq, PartialOrd, Ord, From, FromStr,
-         Into, Hash, Serialize, Deserialize, DieselTypes)]
-pub struct InvoiceId(pub Uuid);
-
-impl InvoiceId {
-    pub fn new() -> Self {
-        InvoiceId(Uuid::new_v4())
-    }
-}
-
-#[derive(Clone, Copy, Debug, Display, Default, PartialEq, Eq, PartialOrd, Ord, From, FromStr,
-         Into, Hash, Serialize, Deserialize, DieselTypes)]
-pub struct SagaId(pub Uuid);
-
-impl SagaId {
-    pub fn new() -> Self {
-        SagaId(Uuid::new_v4())
-    }
-}
-
-#[derive(Clone, Copy, Debug, Display, Default, PartialEq, Eq, PartialOrd, Ord, From, FromStr,
-         Into, Hash, Serialize, Deserialize, DieselTypes)]
-pub struct MerchantId(pub Uuid);
-
-impl MerchantId {
-    pub fn new() -> Self {
-        MerchantId(Uuid::new_v4())
-    }
-}
-
-#[derive(Clone, Copy, Debug, Display, Default, PartialEq, Eq, PartialOrd, Ord, From, FromStr,
-         Into, Hash, Serialize, Deserialize, DieselTypes)]
-pub struct OrderId(pub Uuid);
-
-impl OrderId {
-    pub fn new() -> Self {
-        OrderId(Uuid::new_v4())
-    }
-}
-
-#[derive(Clone, Copy, Debug, Display, Default, PartialEq, Eq, PartialOrd, Ord, From, FromStr,
-         Into, Hash, Serialize, Deserialize, DieselTypes)]
-pub struct OrderInfoId(pub Uuid);
-
-impl OrderInfoId {
-    pub fn new() -> Self {
-        OrderInfoId(Uuid::new_v4())
-    }
-}
-
-#[derive(Clone, Copy, Debug, Display, Default, PartialEq, Eq, PartialOrd, Ord, From, FromStr,
-         Into, Hash, Serialize, Deserialize, DieselTypes)]
-pub struct CallbackId(pub Uuid);
-
-impl CallbackId {
-    pub fn new() -> Self {
-        CallbackId(Uuid::new_v4())
-    }
-}
+uuid_newtype!(StockId);
+uuid_newtype!(InvoiceId);
+uuid_newtype!(SagaId);
+uuid_newtype!(MerchantId);
+uuid_newtype!(OrderId);
+uuid_newtype!(OrderInfoId);
+uuid_newtype!(CallbackId);
 
 #[derive(Clone, Copy, Debug, Default, PartialEq, Eq, PartialOrd, Ord, From, FromStr, Into, Hash,
          Serialize, Deserialize, DieselTypes)]
@@ -153,10 +85,7 @@ impl fmt::Display for CurrencyId {
     }
 }
 
-#[derive(Clone, Copy, Debug, Default, Display, From, FromStr, Into, PartialEq, Serialize,
-         Deserialize, DieselTypes)]
-
-pub struct ProductPrice(pub f64);
+f64_newtype!(ProductPrice);
 
 impl<'a> From<&'a ProductPrice> for f64 {
     fn from(p: &ProductPrice) -> f64 {
@@ -164,12 +93,4 @@ impl<'a> From<&'a ProductPrice> for f64 {
     }
 }
 
-#[derive(Clone, Copy, Debug, Default, Display, From, FromStr, Into, PartialEq, Serialize,
-         Deserialize, DieselTypes)]
-pub struct ConversionId(pub Uuid);
-
-impl ConversionId {
-    pub fn new() -> Self {
-        ConversionId(Uuid::new_v4())
-    }
-}
+uuid_newtype!(ConversionId);

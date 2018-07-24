b426c28de0517621bbd0d63ee688daf4b1e86fd0
Artem Vorotnikov <artem@vorotnikov.me>
2018-07-24 15:34:50 +0300

    Roles refactor (#86)

--- roles/src/routing.rs
+++ roles/src/routing.rs
@@ -1,12 +1,11 @@
 use models::*;
 use service::*;
 
-use failure;
-use futures::future;
 use futures::prelude::*;
 use hyper::{Body, Delete, Get, Method, Post};
 use serde::{de::DeserializeOwned, Serialize};
 use std::rc::Rc;
+use stq_http::controller::ControllerFuture;
 use stq_http::request_util::*;
 use stq_router::RouteParser;
 use stq_types::*;
@@ -47,24 +46,28 @@ impl<T> Controller<T>
 where
     T: Serialize + DeserializeOwned + 'static,
 {
-    pub fn call(&self, method: Method, route: Route, payload: Body) -> Box<Future<Item = String, Error = failure::Error>> {
+    pub fn call(&self, method: &Method, route: &Route, payload: Body) -> Option<ControllerFuture> {
         let service = self.service.clone();
 
         match (method, route) {
-            (Get, Route::RolesByUserId(user_id)) => serialize_future({ service.get_roles_for_user(user_id) }),
-            (Post, Route::Roles) => {
-                serialize_future({ parse_body::<RoleEntry<T>>(payload).and_then(move |data| service.create_role(data)) })
-            }
-            (Delete, Route::RolesByUserId(user_id)) => serialize_future({
-                parse_body::<Option<T>>(payload).and_then(move |role| service.remove_role(RoleSearchTerms::Meta((user_id, role))))
+            (Get, Route::RolesByUserId(user_id)) => Some({
+                let user_id = *user_id;
+                serialize_future({ service.get_roles_for_user(user_id) })
             }),
-            (Delete, Route::RoleById(role_id)) => serialize_future({ service.remove_role(RoleSearchTerms::Id(role_id)) }),
-            // Fallback
-            (other_method, other_route) => Box::new(future::err(format_err!(
-                "Could not route request {} {:?}",
-                other_method,
-                other_route
-            ))),
+            (Post, Route::Roles) => Some(serialize_future({
+                parse_body::<RoleEntry<T>>(payload).and_then(move |data| service.create_role(data))
+            })),
+            (Delete, Route::RolesByUserId(user_id)) => Some({
+                let user_id = *user_id;
+                serialize_future({
+                    parse_body::<Option<T>>(payload).and_then(move |role| service.remove_role(RoleSearchTerms::Meta((user_id, role))))
+                })
+            }),
+            (Delete, Route::RoleById(role_id)) => Some({
+                let role_id = *role_id;
+                serialize_future({ service.remove_role(RoleSearchTerms::Id(role_id)) })
+            }),
+            (_, _) => None,
         }
     }
 }

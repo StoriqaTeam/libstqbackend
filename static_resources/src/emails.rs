a877ffceae6c6e968b980c85902bf5e4ae05778b
Serejka <5125402@mail.ru>
2018-07-31 15:27:10 +0300

    fixed email models

--- static_resources/src/emails.rs
+++ static_resources/src/emails.rs
@@ -1,28 +1,35 @@
 use super::*;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct SendMail {
+pub struct SimpleMail {
     pub to: String,
     pub subject: String,
     pub text: String,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct EmailUser {
+    pub email: String,
+    pub first_name: String,
+    pub last_name: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct OrderUpdateStateForUser {
-    pub user_email: String,
+    pub user: EmailUser,
     pub order_slug: String,
     pub order_state: OrderState,
     pub cluster_url: String,
 }
 
 pub trait Email {
-    fn into_send_mail(self) -> SendMail;
+    fn into_send_mail(self) -> SimpleMail;
 }
 
 impl Email for OrderUpdateStateForUser {
-    fn into_send_mail(self) -> SendMail {
-        SendMail {
-            to : self.user_email,
+    fn into_send_mail(self) -> SimpleMail {
+        SimpleMail {
+            to : self.user.email,
             subject : format!("Your order {} has changed.", self.order_slug),
             text : format!(
                 "Orders' {} state is '{}' now. You can watch current info about your order on <a href=\"{}/profile/orders/{}\">this page</a>.",
@@ -42,8 +49,8 @@ pub struct OrderUpdateStateForStore {
 }
 
 impl Email for OrderUpdateStateForStore {
-    fn into_send_mail(self) -> SendMail {
-        SendMail {
+    fn into_send_mail(self) -> SimpleMail {
+        SimpleMail {
             to: self.store_email,
             subject: format!("Changed orders' {} state.", self.order_slug),
             text: format!(
@@ -56,15 +63,15 @@ impl Email for OrderUpdateStateForStore {
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct EmailVerificationForUser {
-    pub user_email: String,
+    pub user: EmailUser,
     pub verify_email_path: String,
     pub token: String,
 }
 
 impl Email for EmailVerificationForUser {
-    fn into_send_mail(self) -> SendMail {
-        SendMail {
-            to: self.user_email,
+    fn into_send_mail(self) -> SimpleMail {
+        SimpleMail {
+            to: self.user.email,
             subject: "Email verification".to_string(),
             text: format!("{}/{}", self.verify_email_path, self.token),
         }
@@ -73,15 +80,15 @@ impl Email for EmailVerificationForUser {
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct PasswordResetForUser {
-    pub user_email: String,
+    pub user: EmailUser,
     pub reset_password_path: String,
     pub token: String,
 }
 
 impl Email for PasswordResetForUser {
-    fn into_send_mail(self) -> SendMail {
-        SendMail {
-            to: self.user_email,
+    fn into_send_mail(self) -> SimpleMail {
+        SimpleMail {
+            to: self.user.email,
             subject: "Password reset".to_string(),
             text: format!("{}/{}", self.reset_password_path, self.token),
         }
@@ -90,13 +97,13 @@ impl Email for PasswordResetForUser {
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct ApplyPasswordResetForUser {
-    pub user_email: String,
+    pub user: EmailUser,
 }
 
 impl Email for ApplyPasswordResetForUser {
-    fn into_send_mail(self) -> SendMail {
-        SendMail {
-            to: self.user_email,
+    fn into_send_mail(self) -> SimpleMail {
+        SimpleMail {
+            to: self.user.email,
             subject: "Password reset success".to_string(),
             text: "Password for linked account has been successfully reset.".to_string(),
         }
@@ -105,13 +112,13 @@ impl Email for ApplyPasswordResetForUser {
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct ApplyEmailVerificationForUser {
-    pub user_email: String,
+    pub user: EmailUser,
 }
 
 impl Email for ApplyEmailVerificationForUser {
-    fn into_send_mail(self) -> SendMail {
-        SendMail {
-            to: self.user_email,
+    fn into_send_mail(self) -> SimpleMail {
+        SimpleMail {
+            to: self.user.email,
             subject: "Email verification".to_string(),
             text: "Email for linked account has been verified.".to_string(),
         }

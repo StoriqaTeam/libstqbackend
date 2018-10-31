f65649a2a6b2c49cee7924dc50ff9ce68b0f864d
Alexander Andreev <andreevlex.as@gmail.com>
2018-10-31 12:30:09 +0300

    Revert "Fix convert_cart method"
    This reverts commit 376bda3b222c50f44a6f5257d0648b8e177e6d40.

--- api/src/orders.rs
+++ api/src/orders.rs
@@ -891,7 +891,6 @@ pub trait OrderClient {
         address: AddressFull,
         receiver_name: String,
         receiver_phone: String,
-        receiver_email: String,
         coupons: HashMap<CouponId, CouponInfo>,
     ) -> ApiFuture<Vec<Order>>;
     fn create_buy_now(
